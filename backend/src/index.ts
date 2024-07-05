6298ee5acd8554e07bf125a10674bc44b3dca45c
samarth8765 <samdhawan8765@gmail.com>
2024-07-05 23:31:58 +0530

    backend init

---
+++ backend/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import { S3Client, PutObjectCommand, S3 } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+const client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+app.post("/generate-presigned-url", async (req, res) => {
+  try {
+    console.log("Begin");
+    const { fileName, fileType } = req.body;
+    console.log(process.env.BUCKET_NAME);
+    const command = new PutObjectCommand({
+      Bucket: process.env.BUCKET_NAME!,
+      Key: fileName,
+      ContentType: fileType,
+    });
+
+    const url = await getSignedUrl(client, command, {
+      expiresIn: 1800,
+    });
+    console.log("done");
+    return res.status(201).json({ url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error generating presigned URL" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
